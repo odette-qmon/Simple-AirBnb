716001edf2e11b1479086dcddcac9b36825636b5
Odette Quiroz <odette.qmon@gmail.com>
2021-04-28 22:30:14 -0500

    mvc ready

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,29 +1,48 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
   def index
     @flats = Flat.all
   end
 
-  def show
-
-  end
+  def show; end
 
   def new
-
+    @flat = Flat.new
   end
 
   def create
-
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat
+    else
+      render :new
+    end
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
-
+    if @flat.update(flat_params)
+      redirect_to @flat
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @flat.destroy
+    # PREGUNTA!!! por que _url
+    redirect_to flats_url, notice: 'Article was successfully destroyed.'
+  end
 
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
